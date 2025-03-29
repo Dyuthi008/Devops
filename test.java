82a5112cf28cd69d6fd2b73e8d66c2f7ef2e5245
Dyuthi008 <vvce22ise0071@vvce.ac.in>
2025-03-29 12:48:42 +0530

    Update test.java

--- test.java
+++ test.java
@@ -1,6 +1,16 @@
-class test
-{
-public static void main(String[] args) {
-System.out.println("My first java program.");
+ class test {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String original = scanner.nextLine();
+        scanner.close();
+
+        String reversed = new StringBuilder(original).reverse().toString();
+
+         if (original.equalsIgnoreCase(reversed)) {
+            System.out.println("The string is a palindrome!");
+        } else {
+            System.out.println("The string is NOT a palindrome.");
+        }
+    }
 }
-};
